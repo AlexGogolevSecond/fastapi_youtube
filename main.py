8c8f9084387653a1e63546877deaec8e8d50d3de
alex <aaa@mail.ru>
2023-08-05 22:32:25 +0500

    05.08.23 2

--- main.py
+++ main.py
@@ -66,12 +66,9 @@ async def get_hello():
 # endregion
 
 fake_trades = [
-    {"id": 1, "user_id": 1, "currency": "BTC", "side": "buy", "price": 123,
-     "amount": 2.12},
-    {"id": 2, "user_id": 1, "currency": "BTC", "side": "sell", "price": 125,
-     "amount": 2.12},
-    {"id": 3, "user_id": 2, "currency": "BTC", "side": "sell", "price": 124,
-     "amount": 2.2},
+    {"id": 1, "user_id": 1, "currency": "BTC", "side": "buy", "price": 123, "amount": 2.12},
+    {"id": 2, "user_id": 1, "currency": "BTC", "side": "sell", "price": 125, "amount": 2.12},
+    {"id": 3, "user_id": 2, "currency": "BTC", "side": "sell", "price": 124, "amount": 2.2},
 ]
 
 
