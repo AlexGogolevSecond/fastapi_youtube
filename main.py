2a432b8f6e54cb8f172b3bae8f65f620c85b70d2
alex <aaa@mail.ru>
2023-08-05 21:50:12 +0500

    05.08.23

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-from typing import List
+from datetime import datetime
+from enum import Enum
+from typing import List, Optional
 from fastapi import FastAPI
 from pydantic import BaseModel, Field
 
@@ -8,17 +10,32 @@ app = FastAPI(title='Training app')
 fake_users = [
     {'id': 1, 'role': 'admin', 'name': 'Bob'},
     {'id': 2, 'role': 'investor', 'name': 'John'},
-    {'id': 3, 'role': 'trader', 'name': 'Matt'}
+    {'id': 3, 'role': 'trader', 'name': 'Matt'},
+    {'id': 4, 'role': 'investor', 'name': 'Homer', 'degree': [
+        {'id': 1, 'created_at': '2020-01-01T00:00:00', 'type_degree': 'expert'}
+    ]}
 ]
+
+
+class DegreeType(Enum):
+    newbie = 'newbie'
+    expert = 'expert'
+
+
+class Degree(BaseModel):
+    id: int
+    created_at: datetime
+    type_degree: DegreeType
 
 
 class User(BaseModel):
     id: int
     role: str
     name: str
+    degree: Optional[List[Degree]] = []  # если [], то вернёт не null, а пустой список
 
 
-@app.get("/users/{user_id}", response_model=List[User])
+@app.get("/users/{user_id}", response_model=List[User])  # PATH-param
 async def get_user(user_id: int):
     return [user for user in fake_users if user['id'] == user_id]
 
@@ -34,6 +51,10 @@ fake_users2 = [
 async def change_user_name(user_id: int, new_name: str):
     current_user = list(filter(lambda user: user.get("id") == user_id,
                                fake_users2))[0]
+    print(current_user)
+    current_user2 = [x for x in fake_users2 if x.get('id') == user_id][0]
+    print(current_user2)
+
     current_user["name"] = new_name
     return {"status": 200, "data": current_user}
 
@@ -43,16 +64,6 @@ async def get_hello():
     return {"message": "Hello World"}
 
 # endregion
-
-
-class Trade(BaseModel):
-    id: int
-    user_id: int
-    currency: str = Field(max_length=5)
-    side: str
-    price: float = Field(ge=0)
-    amount: float
-
 
 fake_trades = [
     {"id": 1, "user_id": 1, "currency": "BTC", "side": "buy", "price": 123,
@@ -67,6 +78,15 @@ fake_trades = [
 @app.get("/trades")
 def get_trades(limit: int = 1, offset: int = 0):  # QUERY-param
     return fake_trades[offset:][:limit]
+
+
+class Trade(BaseModel):
+    id: int
+    user_id: int
+    currency: str = Field(max_length=5)
+    side: str
+    price: float = Field(ge=0)
+    amount: float
 
 
 @app.post("/trades/")
