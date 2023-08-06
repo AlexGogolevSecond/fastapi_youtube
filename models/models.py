c72e28dc3994dc3bcbd5468280418cfea57e4a9e
alex <aaa@mail.ru>
2023-08-06 16:49:36 +0500

    06.08.23

--- models/models.py
+++ models/models.py
@@ -1,12 +1,26 @@
 
-from sqlalchemy import MetaData, Table, Column, Integer, String, TIMESTAMP, ForeignKey
+from sqlalchemy import JSON, MetaData, Table, Column, Integer, String, TIMESTAMP, ForeignKey
+from datetime import datetime
 
 
 metadata = MetaData()
 
 roles = Table(
     'roles',
-    metadata
+    metadata,
+    Column('id', Integer, primary_key=True),
+    Column('name', String, nullable=False),
+    Column('permissions', JSON),
 )
 
 
+users = Table(
+    'users',
+    metadata,
+    Column('id', Integer, primary_key=True),
+    Column('email', String, nullable=False),
+    Column('username', String, nullable=False),
+    Column('password', String, nullable=False),
+    Column('registered_at', TIMESTAMP, default=datetime.utcnow),
+    Column('role_id', Integer, ForeignKey('roles.id')),
+)
